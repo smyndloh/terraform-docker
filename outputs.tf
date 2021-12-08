c0fb39613bec964bbf11fddec8f74c197df0f646
Smyndlo <greatzlab@gmail.com>
2021-12-08 05:37:36 +0000

    Container Outputs -SORTED

--- outputs.tf
+++ outputs.tf
@@ -7,6 +7,11 @@ output "ip-address" {
   value = flatten(module.container[*].ip_address)
   description = "IP Add & external port of the container"
  }
+ 
+ output "image-name" {
+   value = module.image.image_out
+   description = "Image Name"
+ }
 # output "ip-address" {
 # #   value = join(":", docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external)
 # #   description = "IP Add & external port of the container"
